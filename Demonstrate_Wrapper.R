c6493979574407598c1cf7229048e60eed18932b
mam2737 <mam2737@yahoo.com>
2015-03-06 12:12:07 -0500

    Update Demonstrate_Wrapper.R

--- Demonstrate_Wrapper.R
+++ Demonstrate_Wrapper.R
@@ -1,7 +1,16 @@
+#Allows assignment of arguments
 args <- commandArgs(TRUE)
+
+#assigning dir as the first argument
 dir <- as.character(args[1])
 
-
+#Scripts used for Demonstrate to run
+source("se.R")
+source("lineplotCI.R")
 source("Demonstrate_Stampede_Edition.R")
 
+#Running the actual program
 Demonstrate(dir)
+
+#Command to run program linux
+#Rscript Demonstrate_Wrapper.R ~/Desktop/Demonstrate-Example
