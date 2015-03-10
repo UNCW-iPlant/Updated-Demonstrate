174123a8916dfa9b827a376e273249542d5da9b5
mam2737 <mam2737@yahoo.com>
2015-03-10 09:59:25 -0400

    Update Demonstrate_Wrapper.R

--- Demonstrate_Wrapper.R
+++ Demonstrate_Wrapper.R
@@ -1,9 +1,15 @@
-#Test edit
+# Assignment to the command arguments is being performed here. 
 args <- commandArgs(TRUE)
 dir <- as.character(args[1])
 
-
+# source(file) allows R to read and parse the input until the end of the file is reached. In other words it allows you to use the function by running it. 
 source("Demonstrate_Stampede_Edition.R")
 source("lineplotCI.R")
 source("se.R")
+
+#This is what will be called from the linux command line.
 Demonstrate(dir)
+
+
+#An example would be
+# Rscript Demonstrate_Wrapper.R dir
